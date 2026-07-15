14f1d95079ee4b0a64098749fabc4049ec8455b9
Dunchy <averybgunnoe@gmail.com>
2026-07-15 15:49:51 -0700

    Added JS folder with main.js
    Added an HTML file to be utilized by the main.js file. Added variables, statements, and an expression.

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+// Multiple string variables defined at the same tiem
+var firstGame = "Minecraft",
+    secondGame = "Backyard Baseball",
+    thirdGame = "Knights of the Old Republic",
+    fourthGame = "Apex Legends",
+    fifthGame = "Call of Duty: Warzone";
+
+// String to be concantenated
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
+
+
+document.write(firstGame);
+alert(Sent1 + Sent2);
+
