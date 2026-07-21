c194ef0a0077608202e394e0f87e85ddcb8fd31b
Dunchy <averybgunnoe@gmail.com>
2026-07-21 13:56:41 -0700

    Added commenting to both files

--- Basic-Javascript-Projects/Project4_dictionaries/JS/main.js
+++ Basic-Javascript-Projects/Project4_dictionaries/JS/main.js
@@ -4,6 +4,8 @@ function MyDictionary(){
         heat: "90%",
         flavor: "savoury"
     }
+    // used to delete a key in the dictionary
     delete Pepper.flavor;
+    // retrieves the element with the id display and sets the text to the value inside of the key Pepper.flavor
     document.getElementById("display").innerHTML = Pepper.flavor;
 }
