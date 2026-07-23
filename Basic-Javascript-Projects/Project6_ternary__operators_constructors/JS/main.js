787779f25c057d9111e127248b79b47e9014224e
Dunchy <averybgunnoe@gmail.com>
2026-07-23 15:20:17 -0700

    Fixed some typos
    Changed the auto placed ariaValueMax with just value. Added some required text form the lessons

--- Basic-Javascript-Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic-Javascript-Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,6 +1,16 @@
 function Ride_Function(){
-    var Height, can_ride;
-    Height = document.getElementById("Height").ariaValueMax;
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = can_ride + " to ride."
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride."
+}
+
+function Meme(){
+    var memeNumber = 67
+    document.getElementById("Nested_Function").innerHTML = WithinMeme();
+    function WithinMeme(){
+        memeNumber += 2;
+        return memeNumber;
+    }
+    WithinMeme();
 }
